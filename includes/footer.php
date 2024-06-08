e2541c191290586de4a0cf3a9351eb1e303cfa60
Aditya Sharma <as1657971@gmail.com>
2024-06-08 06:16:49 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,36 @@
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city">
+                <a href="https://www.instagram.com/world.language.club_jit/" target="_blank">World Language Club</a>
+            </div>
+            <div class="footer-city">
+                <a href="https://www.instagram.com/sports.jit/" target="_blank">Sports</a>
+            </div>
+            <div class="footer-city">
+                <a href="https://www.instagram.com/techclubjit/" target="_blank">Tech Club</a> 
+            </div>
+            <div class="footer-city">
+                <a href="https://www.instagram.com/__bitsnbytes__/" target="_blank">BitsNBytes</a>
+            </div>
+            <div class="footer-city">
+                <a href="https://www.instagram.com/crea_jit/" target="_blank">Crea</a>
+            </div>
+            <div class="footer-city">
+                <a href="https://www.instagram.com/edc_jit/" target="_blank">EDC</a>
+            </div>
+            <div class="footer-city">
+                <a href="https://www.instagram.com/__ieee_.jit__/" target="_blank">IEEE</a>
+            </div>
+            <div class="footer-city">
+                <a href="https://www.instagram.com/rotaract.jit/" target="_blank">Rotaract</a>
+            </div>
+            
+        </div>
+        <div class="footer-copyright">© 2024 Copyright KnowYourCollege. All Rights Reserved | Peace</div>
+    </div>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/common.js"></script>
